1cea5cf5a34615d915870026057172186c51576c
root <root@kali.simphiwe>
2023-01-23 09:10:14 +0200

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ *bubble_sort - sort an array using bubble sort algo
+ *@param array - unsorted integer array
+ *@param size - size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+
+	size_t i, j;
+	int temp;
+	int checker;
+
+
+	for (j = 0; j < 1024 ; j++)
+	{
+	checker = 0;
+	for (i = 0; i < size - 1; i++)
+	{
+
+		if (array[i] >  array[i + 1])
+		{
+			temp = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = temp;
+			i += 2;
+			checker = 1;
+		}
+	}
+	print_array(array, size);
+	if (checker == 0)
+	{
+		printf("\n"); 
+		exit(EXIT_SUCCESS);
+	}
+}
+}
